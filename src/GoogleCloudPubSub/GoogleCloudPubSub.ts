e60526c910e060b3b15fe1012eff2c7aaf2ef5c0
Huy Dinh <huydinh.hcmus@gmail.com>
2021-01-12 10:48:05 +0100

    fix: use spreading operator instead of instanciating a new extendedMessage

--- src/GoogleCloudPubSub/GoogleCloudPubSub.ts
+++ src/GoogleCloudPubSub/GoogleCloudPubSub.ts
@@ -127,12 +127,9 @@ export class GoogleCloudPubSub implements GCPubSub {
 
     subscription.on('message', (message: Message): void => {
       const extendedMessage: ExtendedMessage<T> = new ExtendedMessage<T>(message);
-      const logMessage: ExtendedMessage<T> = new ExtendedMessage<T>(message);
 
-      // eslint-disable-next-line
-      delete (logMessage as any).originalMessage;
       this.logger.debug(
-        logMessage,
+        { ...extendedMessage, originalMessage: undefined },
         `A message has been received for Subscription ${subscription.name} after ${
           message.received - message.publishTime.valueOf()
         } ms`,
