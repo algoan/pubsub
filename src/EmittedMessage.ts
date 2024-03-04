be3c53b24bb48cbc3ece21e09dd767a90afc79f1
ccoeurderoy <charlescoeurderoy@gmail.com>
2024-03-04 19:25:34 +0100

    feat(emitted-message)!: redefine the emitted message interface

--- src/EmittedMessage.ts
+++ src/EmittedMessage.ts
@@ -1,20 +1,17 @@
-import { Message } from '@google-cloud/pubsub';
-import { ErrorPayload } from './GoogleCloudPubSub';
+import { AnyProperties } from './lib';
 
 /**
  * Message emitted by the PubSub
  */
-export interface EmittedMessage<T> {
+export interface EmittedMessage {
   /** Message unique identifier */
   id: string;
   /** Payload sent */
-  payload: T | ErrorPayload;
+  payload: AnyProperties;
   /** Metadata: namespace, environment etc */
-  metadata?: Metadata;
+  metadata?: AnyProperties<string>;
   /** Acknowledgment unique identifier */
   ackId?: string;
-  /** Counter, if message are ordered */
-  count?: number;
   /** Date of emission */
   emittedAt: Date;
   /** Date of reception */
@@ -23,30 +20,15 @@ export interface EmittedMessage<T> {
   duration: number;
   /** Ordering Key */
   orderingKey?: string;
+  /** Number of attempts to deliver the message */
+  deliveryAttempt?: number;
   /** Acknowledge method for pubsub providers with a manual acknowledgment */
-  ack(): void;
-  /** Modify Acknowledge method for pubsub providers with a manual acknowledgment */
+  ack(): Promise<void> | void;
+  /** Modify Acknowledge method for pubsub providers with a manual acknowledgment. Deadline in seconds */
   modAck(deadline: number): void;
   /** Not-Acknowledge method for pubsub providers with a manual acknowledgment */
-  nack(): void;
+  nack(): Promise<void> | void;
   /** Getter for retrieving the original message for custom use case */
-  getOriginalMessage(): Message;
-}
-
-/**
- * Payload metadata
- */
-export interface Metadata {
-  namespace?: string;
-  environment?: string;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [key: string]: any;
+  getOriginalMessage(): any;
 }
-
-/**
- * Check if a message is in an error state
- * @param payload
- */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/tslint/config
-export const isPayloadError = <T extends object>(payload: T | ErrorPayload): payload is ErrorPayload =>
-  'code' in payload && 'err' in payload;
